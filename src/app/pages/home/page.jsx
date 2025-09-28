ecba7cea8b6dfcab09087a048892ec055ea22ab0
Zain Ali <zainaliworkmail3830@gmail.com>
2025-09-28 04:48:12 -0700

    Add movie crousal

---
+++ src/app/pages/home/page.jsx
@@ -0,0 +1,19 @@
+'use client';
+import Navbar from "@/app/components/navbar";
+import Slider from "@/app/components/slider";
+function HomePage() {
+  return (
+    <>
+   <div className=" bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800">
+     <div className="flex flex-col gap-4 items-center justify-center bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800">
+        <Navbar />
+      <h1 className="text-4xl font-bold text-white text-center px-1">Movie Night Starts Here</h1>
+    </div>
+          <Slider />
+   </div>
+    </>
+
+  );
+}
+
+export default HomePage;
