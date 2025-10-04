375dd8bad9847fb3a3ca5e46268f3d970091a2a3
Zain Ali <zainaliworkmail3830@gmail.com>
2025-10-04 10:56:12 -0700

    add toater in signup page

--- src/app/pages/signup/page.jsx
+++ src/app/pages/signup/page.jsx
@@ -1,29 +1,44 @@
-'use client';
+"use client";
 import SignupForm from "@/app/components/SignupForm";
 import { useRouter as Router } from "next/navigation";
 import { signupUser } from "@/app/API's/api.js";
+import { Toaster, toast } from "react-hot-toast";
 function SignupPage() {
-    const router=Router()
-  const handleSignup = async (formData,setFormData) => {
-    console.log("Signing up with", formData.userName, formData.email, formData.password,formData.confirmPassword);
-    const res= await signupUser(formData)
-    if(res){
-       setFormData({
-      userName: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    });
-        router.push('/pages/login')
+  const router = Router();
+  const handleSignup = async (formData, setFormData) => {
+    console.log(
+      "Signing up with",
+      formData.userName,
+      formData.email,
+      formData.password,
+      formData.confirmPassword
+    );
+    try {
+      const res = await signupUser(formData);
+      if (res) {
+        setFormData({
+          userName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        });
+        toast.success("Signup successful! Please login.");
+        router.push("/pages/login");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message || "Invalid credentials");
     }
   };
 
   return (
-    <div className="min-h-screen px-4 py-1 flex items-center justify-center bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800">
-      <SignupForm onSubmit={handleSignup} />
-    </div>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="min-h-screen px-4 py-1 flex items-center justify-center bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800">
+        <SignupForm onSubmit={handleSignup} />
+      </div>
+    </>
   );
 }
 
 export default SignupPage;
-
